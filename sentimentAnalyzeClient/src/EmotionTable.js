0ff297ced42d387c58d891242de548f375734e51
Deepali <dipalij@skillup.tech>
2022-03-10 16:00:17 +0530

    seacond commit

---
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './bootstrap.min.css';
+
+class EmotionTable extends React.Component {
+    render() {
+        const emotionstomap = this.props.emotions
+        const mappingemotions = Object.keys(emotionstomap).map(ems =>
+            {
+                console.log(ems)
+                return(
+                    <>
+                    <tr>
+                    <td>{ems}</td>
+                    <td>{emotionstomap[ems]}</td>
+                    </tr>
+                    </>
+                )
+            })
+      return (  
+        <div>
+
+          <table className="table table-bordered">
+            <tbody>
+            {mappingemotions}
+            </tbody>
+          </table>
+          </div>
+          );
+        }
+    
+}
+export default EmotionTable;
---
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -0,0 +1,131 @@
+const express = require('express');
+const dotenv = require('dotenv')
+const nluinstancecreator = require('./Middleware/IBMNewInstanceCreator')
+dotenv.config()
+
+const app = new express();
+
+app.use(express.static('client'))
+
+const cors_app = require('cors');
+app.use(cors_app());
+
+app.get("/",(req,res)=>{
+    res.render('index.html');
+  });
+
+app.get("/url/emotion", (req,res) => {
+    const querytext = req.query.url
+    const newinstance = nluinstancecreator
+    const analyzeparams = {
+        'url': querytext,
+        'features': {
+            'entities' : {
+                'emotion' : true,
+                'sentiment': false
+            }, 'keywords': {
+                'emotion': true,
+                'sentiment': false
+
+            }
+        } 
+    }
+
+    newinstance.analyze(analyzeparams).then(analysisresults =>
+        {
+           const emotionalanalysis = analysisresults.result.entities[0].emotion
+            return res.send({emotions: emotionalanalysis});
+        }).catch( err =>
+            {
+                console.log(err)
+            })
+});
+
+app.get("/url/sentiment", (req,res) => {
+    const querytext = req.query.url
+    const newinstance = nluinstancecreator
+    let sentimentresponse
+    const analyzeparams = {
+        'url': querytext,
+        'features': {
+            'entities' : {
+                'sentiment' : true,
+                'emotion'  : false
+            }, 'keywords': {
+                'sentiment': true,
+                'emotion': false
+
+            }
+        } 
+    }
+
+    newinstance.analyze(analyzeparams).then(analysisresults =>
+        {
+            console.log(JSON.stringify(analysisresults, null, 2))
+            sentimentresponse = analysisresults.result.entities[0].sentiment.label
+            return res.send({senti: sentimentresponse});
+        }).catch( err =>
+            {
+                console.log(err)
+            })
+});
+
+app.get("/text/emotion", (req,res) => {
+    const querytext = req.query.text
+    const newinstance = nluinstancecreator
+    const analyzeparams = {
+        'text': querytext,
+        'features': {
+            'entities' : {
+                'emotion' : true,
+                'sentiment': false
+            }, 'keywords': {
+                'emotion': true,
+                'sentiment': false
+
+            }
+        } 
+    }
+
+    newinstance.analyze(analyzeparams).then(analysisresults =>
+        {
+            const emotionalanalysis = analysisresults.result.entities[0].emotion
+            return res.send({emotions: emotionalanalysis});
+        }).catch( err =>
+            {
+                console.log(err)
+            })
+});
+
+app.get("/text/sentiment", (req,res) => {
+    const querytext = req.query.text
+    const newinstance = nluinstancecreator
+    const analyzeparams = {
+        'text': querytext,
+        'features': {
+            'entities' : {
+                'sentiment' : true,
+                'emotion'  : false
+            }, 'keywords': {
+                'sentiment': true,
+                'emotion': false
+
+            }
+        } 
+    }
+
+    newinstance.analyze(analyzeparams).then(analysisresults =>
+        {
+            console.log(JSON.stringify(analysisresults, null, 2))
+            const sentimentresponse = analysisresults.result.entities[0].sentiment.label
+            return res.send({senti: sentimentresponse});
+        }).catch( err =>
+            {
+                console.log(err)
+            })
+});
+
+let server = app.listen(8080, () => {
+    console.log('Listening', server.address().port)
+})
+
